073e193ff3839e92a48fe102656569426f249276
Vinicius Carneiro Charleaux <51250383+ViniciusCharleaux@users.noreply.github.com>
2021-09-05 15:31:23 -0300

    Requisição na api do GitHub

--- 01-fundamentos-react/webpack.config.js
+++ 01-fundamentos-react/webpack.config.js
@@ -18,27 +18,18 @@ module.exports = {
     },
     devServer:{
         static: path.resolve(__dirname, 'public'),
-        hot: true,
     },
     plugins:[
-        isDevelopment && new ReactRefreshWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
         })
-    ].filter(Boolean),
+    ],
     module:{
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options:{
-                        plugins: [
-                            isDevelopment && require.resolve('react-refresh/babel')
-                        ].filter(Boolean)
-                    }
-                },
+                use: 'babel-loader',
             },
             {
                 test: /\.scss$/,
