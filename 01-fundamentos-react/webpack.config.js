2fe4bb723a97d5f42fbe003d3039cea71510d791
Vinicius Carneiro Charleaux <51250383+ViniciusCharleaux@users.noreply.github.com>
2021-09-04 21:00:41 -0300

    finalizando configuração

--- 01-fundamentos-react/webpack.config.js
+++ 01-fundamentos-react/webpack.config.js
@@ -1,8 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index,js'),
+    mode:'development',
+    devtool: isDevelopment ? 'eval-source-map' : 's',
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output:{
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
@@ -10,12 +15,25 @@ module.exports = {
     resolve:{
         extensions: ['.js', '.jsx'],
     },
+    devServer:{
+        static: path.resolve(__dirname, 'public'),
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
     module:{
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             }
         ],
     } 
