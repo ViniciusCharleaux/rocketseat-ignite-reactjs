de1d48f516eb27985eaf1fce67e2dda5f2244397
Vinicius Carneiro Charleaux <51250383+ViniciusCharleaux@users.noreply.github.com>
2021-09-10 23:47:38 -0300

    estruturando a aplicação

---
+++ 02-primeira-aplicacao/dtmoney/src/styles/global.ts
@@ -0,0 +1,61 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+    :root{
+        --background: #F0F2F5;
+        --red: #E62E4D;
+        --blue: #5429CC;
+
+        --blue-light: #6933FF;
+        --text-title: #363f5f;
+        --text-body: #969cb3;
+
+        --background: #F0F2F5;
+        --shape: #FFFFFF;
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    //tamanho padrão: 16px
+    //1rem = 16px => depende do tamanho da fonte
+    //porcentagem na fonte vai de acordo com os gostos do usuario, acessibilidade
+
+    html{
+        @media (max-width: 1080px){
+            font-size: 93.75%;  //15px
+        }
+
+        @media (max-width: 720px){
+            font-size: 97.5%;   //14px
+        }
+    }
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+    
+    h1, h2, h3, h4, h5, h6, strong{
+        font-weight: 600;
+    }
+
+    button{
+        cursor: pointer;
+    }
+
+    [disabled]{
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+
+`
