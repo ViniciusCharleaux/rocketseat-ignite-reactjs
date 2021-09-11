03868c98204cb0eea9bb1b68199ead4fb42ee1c2
Vinicius Carneiro Charleaux <51250383+ViniciusCharleaux@users.noreply.github.com>
2021-09-11 14:05:05 -0300

    iniciando modal da aplicação

--- 02-primeira-aplicacao/dtmoney/src/App.tsx
+++ 02-primeira-aplicacao/dtmoney/src/App.tsx
@@ -3,11 +3,34 @@ import { Header } from "./components/Header.tsx";
 
 import { GlobalStyle } from "./styles/global";
 
+import Modal from 'react-modal';
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewModalTransactionOpen, setIsNewModalTransactionOpen] = useState(false);
+
+    function handleOpenNewTransactionModal(){
+        setIsNewModalTransactionOpen(true);
+    }
+
+    function handleCloseNewTransactionModal(){
+        setIsNewModalTransactionOpen(false);
+    }
+
   return (
     <>
-      <Header />
+      <Header 
+        onOpenNewTransactionModal={handleOpenNewTransactionModal}
+      />
+
+      <NewTransactionModal 
+        isOpen={isNewModalTransactionOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <Dashboard />
       <GlobalStyle/>
     </>
