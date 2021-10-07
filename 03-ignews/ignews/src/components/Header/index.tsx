80b8124023a2912bea8629c638d9f590b713e4c5
Vinicius Carneiro Charleaux <51250383+ViniciusCharleaux@users.noreply.github.com>
2021-10-06 21:43:32 -0300

    finalizando componentes da aplicação

--- 03-ignews/ignews/src/components/Header/index.tsx
+++ 03-ignews/ignews/src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
 
@@ -11,6 +12,8 @@ export function Header(){
                     <a className={styles.active} href="">Home</a>
                     <a href="">Posts</a>
                 </nav>
+
+                <SignInButton />
             </div>
         </header>
     )
