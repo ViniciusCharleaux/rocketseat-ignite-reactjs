c0416286de5a8aca61eb35709fde05ca479b98e6
Vinicius Carneiro Charleaux <51250383+ViniciusCharleaux@users.noreply.github.com>
2021-10-07 22:33:35 -0300

    aplicando autenticação com github

--- 03-ignews/ignews/src/components/SignInButton/index.tsx
+++ 03-ignews/ignews/src/components/SignInButton/index.tsx
@@ -1,20 +1,25 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
+import { signIn, signOut, useSession } from 'next-auth/client';
+
 import styles from './styles.module.scss';
 
 export function SignInButton(){
 
-    const isUserLoggedIn = true;
+    const [session] = useSession();
 
-    return isUserLoggedIn ? (
+    console.log(session);
+
+    return session ? (
 
         <button 
             type="button"
-            className={styles.signInButton}    
+            className={styles.signInButton}
+            onClick={() => signOut()}
         >   
             <FaGithub color="#04d361"/>
-            Vinicius Charleaux
+            {session.user.name}
             <FiX 
                 color="#737380"
                 className={styles.closeIcon}
@@ -25,7 +30,8 @@ export function SignInButton(){
 
         <button 
             type="button"
-            className={styles.signInButton}    
+            className={styles.signInButton}
+            onClick={() => signIn('github')} 
         >   
             <FaGithub color="#eba417"/>
             Sign in with Github
--- 03-ignews/ignews/src/pages/index.tsx
+++ 03-ignews/ignews/src/pages/index.tsx
@@ -1,10 +1,18 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
+
+/*tipos de chamadas api
+
+  -Client-side    carregado do lado do cliente
+  -Server-side    carregado do lado do servidor, demora mais pra carregar pro cliente
+  -Static         n é necessário ser em tempo real
+
+*/
 
 interface HomeProps {
   product : {
@@ -43,7 +51,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
     const price = await stripe.prices.retrieve('price_1Jhle5GKg63m3LXgqk02HEAY');
 
@@ -59,6 +67,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
     return {
       props: {
         product
-      }
+      },
+      revalidate: 60 * 60 * 24, //24 hours
     }
 }
