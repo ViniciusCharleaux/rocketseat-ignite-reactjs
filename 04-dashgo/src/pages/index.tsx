1239f8e4a45427491f1e5dcb6f8267f8c8ebd747
Vinicius Carneiro Charleaux <vinicius.charleaux@gmail.com>
2022-07-15 19:26:34 -0300

    componentes de Input e Header

--- 04-dashgo/src/pages/index.tsx
+++ 04-dashgo/src/pages/index.tsx
@@ -1,7 +1,42 @@
+import {Button, Flex, Stack, FormLabel, FormControl} from '@chakra-ui/react'
 
+import {Input} from '../components/Form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <h1>Dashgoa</h1>
-  )
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      > 
+        <Stack spacing={4}>
+
+        <Input
+            label="E-mail"
+            name="email"
+            type="email"
+        />
+
+        <Input
+            label="Senha"
+            name="password"
+            type="password"
+        />
+        
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
+  ) 
 }
